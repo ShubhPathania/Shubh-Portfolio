7088f8a10df626e8db3d1d7d0c7e4281e29d54c2
Shubham Pathania <Shubhpathania619@gmail.com>
2024-08-28 16:52:09 +0530

    Finished Project

---
+++ src/Sections/Projects/Projects.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styles from './ProjectsStyles.module.css';
+import zomato from '../../assets/zomato.png';
+import bike from '../../assets/bike.png';
+import playstore from '../../assets/playstore.png';
+import netflix from '../../assets/netflix.png';
+import customer from '../../assets/customer.png';
+import sales from '../../assets/sales.png';
+import ProjectCard from '../../Common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={zomato}
+          link="https://github.com/ShubhPathania/Zomato-Restaurant-Clustering-and-Sentiment-Analysis"
+          h3="Zomato Restaurant Clustering and Sentiment Analysis"
+          p="Unsupervised Learning Project"
+        />
+        <ProjectCard
+          src={bike}
+          link="https://github.com/ShubhPathania/Bike-Sharing-Demand-Prediction"
+          h3="Bike Sharing Demand Prediction"
+          p="Supervised Learning Project"
+        />
+        <ProjectCard
+          src={playstore}
+          link="https://github.com/ShubhPathania/Play-Store-App-Review-Analysis"
+          h3="Play Store App Review Analysis"
+          p="EDA Project"
+        />
+        <ProjectCard
+          src={netflix}
+          link="https://github.com/ShubhPathania/Product-Dissection-for-Netflix"
+          h3="Product Dissection for Netflix"
+          p="Case Study"
+        />
+        <ProjectCard
+          src={customer}
+          link="https://github.com/ShubhPathania/Customer-Purchase-Behavior-Analysis"
+          h3="Customer Purchase Behavior Analysis"
+          p="Statistics Project"
+        />
+        <ProjectCard
+          src={sales}
+          link="https://github.com/ShubhPathania/AdventureWorks-Sales-Analysis-Excel"
+          h3="Adventure Works Sales Analysis"
+          p="Excel Project"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
---
+++ src/Sections/Skills/Skills.jsx
@@ -0,0 +1,41 @@
+import styles from './SkillsStyles.module.css';
+import checkMarkIconDark from '../../assets/checkmark-dark.svg';
+import checkMarkIconLight from '../../assets/checkmark-light.svg';
+import SkillList from '../../common/SkillList';
+import { useTheme } from '../../Common/ThemeContext';
+
+function Skills() {
+  const { theme } = useTheme();
+  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Python" />
+        <SkillList src={checkMarkIcon} skill="SQL" />
+        <SkillList src={checkMarkIcon} skill="Tableau" />
+        <SkillList src={checkMarkIcon} skill="Power BI" />
+        <SkillList src={checkMarkIcon} skill="Excel" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Statistics" />
+        <SkillList src={checkMarkIcon} skill="Machine Learning" />
+        <SkillList src={checkMarkIcon} skill="NLP" />
+        <SkillList src={checkMarkIcon} skill="Deep Learning" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Numpy" />
+        <SkillList src={checkMarkIcon} skill="Pandas," />
+        <SkillList src={checkMarkIcon} skill="Matplotlib," />
+        <SkillList src={checkMarkIcon} skill="Seaborn," />
+        <SkillList src={checkMarkIcon} skill="Plotly," />
+      </div>
+
+    </section>
+  );
+}
+
+export default Skills;
