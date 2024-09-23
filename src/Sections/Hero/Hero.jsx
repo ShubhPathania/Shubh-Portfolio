09cf8d1b377717da7f1127a82397f8ea0edd738d
Shubham Pathania <Shubhpathania619@gmail.com>
2024-09-24 01:49:25 +0530

    Update Hero.jsx

--- src/Sections/Hero/Hero.jsx
+++ src/Sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/hero image 2.png';
+import heroImg from '../..assets/Hero'
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import githubLight from '../../assets/github-light.svg';
