4cf9f0f124e161ecb29a274cb3bf644e36501ed4
Shubham Pathania <Shubhpathania619@gmail.com>
2025-01-03 23:48:59 +0530

    Update Footer.jsx

--- src/Sections/Footer/Footer.jsx
+++ src/Sections/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 Shubham Pathania. <br />
+        &copy; 2025 Shubham Pathania. <br />
         All rights reserved.
       </p>
     </section>
